d4f5519e781a10552e9d0f7f95099efa174de5c8
abolfazlsadeqi2001 <abolfazlsadeqi2001@chmail.ir>
2020-03-12 08:34:19 +0330

    add unique and autoincrement tests

--- docker-host/src/test/java/main/general/database/mysql/MysqlConnectorTest.java
+++ docker-host/src/test/java/main/general/database/mysql/MysqlConnectorTest.java
@@ -2,18 +2,26 @@ package main.general.database.mysql;
 
 import java.sql.ResultSet;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import main.general.database.mysql.MysqlConnector;
 
 public class MysqlConnectorTest {
-	// TODO check auto increment up
 	// TODO check unique up
 	private final String[] phones = new String[] {"1111","2222"};
 	private final String[] passwords = new String[] {"aa11","bb22"};
 	private final String[] names = new String[] {"abolfazl","alis"};
 	private final String[] families = new String[] {"abolfazl","alis"};
 	private final int[] ages = new int[] {12,14};
+	
+	@BeforeEach
+	public void beforeEach() throws Exception {
+		String deleteTemplate = "DELETE FROM users WHERE telephone like '%s'";
+		for (String i : phones) {
+			MysqlConnector.set(String.format(deleteTemplate, i));
+		}
+	}
 	
 	@Test
 	public void testSet() throws Exception {
@@ -51,5 +59,50 @@ public class MysqlConnectorTest {
 		MysqlConnector.set(deleteQuery);
 		
 		set.close();
+	}
+	
+	/**
+	 * this method first insert two row then get them the id property must changed
+	 * @throws Exception
+	 */
+	@Test
+	public void testAutoIncrementOfId() throws Exception {
+		String insertTemplate = "INSERT INTO users(telephone,password,name,family,age) VALUES('%s','%s','%s','%s','%s')";
+		String selectTemplate = "SELECT id FROM users WHERE telephone='%s'";
+		
+		String insertQuery1 = String.format(insertTemplate, phones[0],passwords[0],names[0],families[0],ages[0]);
+		MysqlConnector.set(insertQuery1);
+		String insertQuery2 = String.format(insertTemplate, phones[1],passwords[1],names[1],families[1],ages[1]);
+		MysqlConnector.set(insertQuery2);
+		
+		String selectQuery1 = String.format(selectTemplate, phones[0]);
+		ResultSet set1 = MysqlConnector.get(selectQuery1);
+		set1.next();
+		
+		String selectQuery2 = String.format(selectTemplate, phones[1]);
+		ResultSet set2 = MysqlConnector.get(selectQuery2);
+		set2.next();
+		
+		if(set1.getInt("id") >= set2.getInt("id"))
+			throw new Exception("id wasn't increased");
+	}
+	
+	@Test
+	public void testUniqueOfTelephone() throws Exception {
+		String insertTemplate = "INSERT INTO users(telephone,password,name,family,age) VALUES('%s','%s','%s','%s','%s')";
+		// insert first element by telephone equals index 0
+		String insertQuery1 = String.format(insertTemplate, phones[0],passwords[0],names[0],families[0],ages[0]);
+		MysqlConnector.set(insertQuery1);
+		// insert second element by telephone equals index 0 must handle it
+		boolean isHasException = false;
+		try {
+			String insertQuery2 = String.format(insertTemplate, phones[0],passwords[1],names[1],families[1],ages[1]);
+			MysqlConnector.set(insertQuery2);
+		} catch (Exception e) {
+			isHasException = true;
+		}
+		if(!isHasException) {
+			throw new Exception("duplicated telephone elements added to database (telephone must be unique)");
+		}
 	}
 }
