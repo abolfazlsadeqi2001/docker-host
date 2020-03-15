fb65efa352c13cb7cbc666cf77f929fb1bb82796
abolfazlsadeqi2001 <abolfazlsadeqi2001@chmail.ir>
2020-03-15 09:49:03 +0330

    remove Ignore from selenium test to prevent attentions

--- docker-host-selenium-integration-test/src/test/java/authentication/Index.java
+++ docker-host-selenium-integration-test/src/test/java/authentication/Index.java
@@ -7,7 +7,6 @@ import java.util.Set;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
